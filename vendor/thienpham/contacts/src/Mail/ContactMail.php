dc1dd3cfdb7615ece5ab4af16d9b4f58de800ff2
KhanhTran <cnkhanhdl@gmail.com>
2018-07-29 08:08:29 +0700

    kt

---
+++ vendor/thienpham/contacts/src/Mail/ContactMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ThienPham\Contacts\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
+        ->subject('Contact Notification')
+        ->view('contacts::email-template');
+    }
+}
