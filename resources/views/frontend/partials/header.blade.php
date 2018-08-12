cb693e38b5b7adb3511762808065a4db132a3b75
KhanhTran <cnkhanhdl@gmail.com>
2018-08-12 10:05:46 +0700

    cm

--- resources/views/frontend/partials/header.blade.php
+++
@@ -1,6 +0,0 @@
-<a href="{{ route('frontend.pages.home') }}">
-    <img class="img-responsive mb-50" src="{{ Voyager::image(setting('site.logo')) }}" alt="">
-</a>
-<nav id="colorlib-main-menu" role="navigation">
-    {{ menu('frontend', 'frontend.partials.main-menu') }}
-</nav>
--- vendor/thienpham/contacts/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-Route::group(['middleware' => ['web']], function () {
-	Route::post('contacts', [
-		'uses' => 'ThienPham\Contacts\Http\Controllers\ContactsController@contactStore',
-		'as'	=> 'store.thienpham.contact'
-	]);
-});
